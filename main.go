3443a3113940d47abf922ebc8d6e6834f457ce2e
rightlag <rightlag@gmail.com>
2017-01-14 17:04:54 -0500

    Add unit test for Swagger schema initialization

--- main.go
+++ main.go
@@ -2,21 +2,13 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 
 	"github.com/rightlag/go-swagger-object-model"
 )
 
-func main() {
-	schema, err := NewSchema("http://petstore.swagger.io/v2/swagger.json")
-	if err != nil {
-		return
-	}
-	// fmt.Printf("%#v\r\n", schema.Paths["/pet"].Put.Responses["400"])
-	fmt.Printf("%#v\r\n", schema)
-}
+func main() {}
 
 func NewSchema(url string) (*models.Schema, error) {
 	schema, err := load(url)
