8adefe828fbab9a9cb9ab84153816c650e99c483
rightlag <rightlag@gmail.com>
2017-01-14 12:39:17 -0500

    Add 'Load' function to fetch schema

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, Slate!")
+	schema, err := Load("http://petstore.swagger.io/v2/swagger.json")
+	if err != nil {
+		return
+	}
+	fmt.Printf("%#v\r\n", schema)
+}
+
+func Load(url string) (interface{}, error) {
+	var schema interface{}
+	response, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+	jsonString, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+	json.Unmarshal(jsonString, &schema)
+	if err != nil {
+		return nil, err
+	}
+	return schema, nil
 }
