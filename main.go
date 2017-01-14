4e844c840639d9bc1265b130b8e3eae7b4689822
rightlag <rightlag@gmail.com>
2017-01-14 12:54:39 -0500

    Create 'Schema' struct type

--- main.go
+++ main.go
@@ -7,16 +7,42 @@ import (
 	"net/http"
 )
 
+type Schema struct {
+	Swagger             string      `json:"swagger"`
+	Info                interface{} `json:"info"`
+	Host                string      `json:"host,omitempty"`
+	BasePath            string      `json:"basePath,omitempty"`
+	Schemes             []string    `json:"schemes,omitempty"`
+	Consumes            []string    `json:"consumes,omitempty"`
+	Produces            []string    `json:"produces,omitempty"`
+	Paths               interface{} `json:"paths"`
+	Definitions         interface{} `json:"definitions,omitempty"`
+	Parameters          interface{} `json:"parameters,omitempty"`
+	Responses           interface{} `json:"responses,omitempty"`
+	SecurityDefinitions interface{} `json:"securityDefinitions,omitempty"`
+	Security            interface{} `json:"security,omitempty"`
+	Tags                interface{} `json:"tags,omitempty"`
+	ExternalDocs        interface{} `json:"externalDocs,omitempty"`
+}
+
 func main() {
-	schema, err := Load("http://petstore.swagger.io/v2/swagger.json")
+	schema, err := NewSchema("http://petstore.swagger.io/v2/swagger.json")
 	if err != nil {
 		return
 	}
 	fmt.Printf("%#v\r\n", schema)
 }
 
-func Load(url string) (interface{}, error) {
-	var schema interface{}
+func NewSchema(url string) (*Schema, error) {
+	schema, err := load(url)
+	if err != nil {
+		return nil, err
+	}
+	return schema, nil
+}
+
+func load(url string) (*Schema, error) {
+	var schema Schema
 	response, err := http.Get(url)
 	if err != nil {
 		return nil, err
@@ -30,5 +56,5 @@ func Load(url string) (interface{}, error) {
 	if err != nil {
 		return nil, err
 	}
-	return schema, nil
+	return &schema, nil
 }
