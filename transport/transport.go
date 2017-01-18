8ba78beace0193ed347abedaeb4717bfca79ab59
rightlag <rightlag@gmail.com>
2017-01-17 21:38:41 -0500

    Add goroutine in Command execute

--- transport/transport.go
+++ transport/transport.go
@@ -1,7 +1,9 @@
 package transport
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"net/url"
 	"strings"
 
@@ -15,6 +17,10 @@ type SshTransport struct {
 type Command struct {
 	args []string
 	*SshTransport
+}
+
+type Response struct {
+	stdout string
 }
 
 func NewSshTransport(uri, password string) (*SshTransport, error) {
@@ -50,12 +56,34 @@ func (c *Command) String() string {
 	return strings.Join(c.args, " ")
 }
 
-func (c *Command) execute() (string, error) {
+func (c *Command) execute() (*Response, error) {
 	session, err := c.NewSession()
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	defer session.Close()
-	stdout, err := session.Output(c.String())
-	return string(stdout), nil
+	stdout, err := session.StdoutPipe()
+	if err != nil {
+		return nil, err
+	}
+	var buf bytes.Buffer
+	if err := session.Start(c.String()); err != nil {
+		return nil, err
+	}
+	done := make(chan bool, 1)
+	go func() {
+		if _, err := io.Copy(&buf, stdout); err != nil {
+			return
+		}
+		done <- true
+	}()
+	if err := session.Wait(); err != nil {
+		return nil, err
+	}
+	<-done
+	return &Response{buf.String()}, nil
+}
+
+func (r *Response) String() string {
+	return string(r.stdout)
 }
