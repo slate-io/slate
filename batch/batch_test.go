96c93c29844ce374e16b492b78c6f79c0bf43e18
Brandon Him <brh55@drexel.edu>
2017-01-26 01:25:29 -0800

    Add batch test

---
+++ batch/batch_test.go
@@ -0,0 +1,67 @@
+package batch
+
+import (
+	"github.com/rightlag/slate/command"
+	"testing"
+)
+
+func TestCommandBatch(t *testing.T) {
+	ls := command.NewCommand("ls -al")
+
+	b := NewBatchCommand()
+	b.Add(ls)
+	if b.commands[0] != ls {
+		t.Error()
+	}
+}
+
+func TestCommandsBatch(t *testing.T) {
+	pwd := command.NewCommand("pwd")
+	ps := command.NewCommand("ps", "-L")
+	cs := []*command.Command{
+		pwd,
+		ps,
+	}
+
+	b := NewBatchCommand()
+	b.Add(cs)
+	if len(b.commands) != 2 {
+		t.Error()
+	}
+	if b.commands[1] != ps {
+		t.Error()
+	}
+}
+
+func TestStringBatch(t *testing.T) {
+	ls := command.NewCommand("ls")
+
+	b := NewBatchCommand()
+	b.Add("ls")
+	if b.commands[0] != ls {
+		t.Error()
+	}
+}
+
+func TestStringsBatch(t *testing.T) {
+	cd := command.NewCommand("cd")
+	ls := command.NewCommand("ls -al")
+	cs := []string{
+		"cd",
+		"ls -al",
+	}
+
+	b := NewBatchCommand()
+	b.Add(cs)
+	if len(b.commands) != 2 {
+		t.Error()
+	}
+	fmt.Print(b.commands)
+
+	if b.commands[0] != cd {
+		t.Error()
+	}
+	if b.commands[1] != ls {
+		t.Error()
+	}
+}
