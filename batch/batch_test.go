45395ae6d49fe129666ae01a693fb96717106136
rightlag <rightlag@gmail.com>
2017-01-28 11:18:35 -0500

    Modify `BatchCommand.Add` functionality

--- batch/batch_test.go
+++ batch/batch_test.go
@@ -1,68 +1,36 @@
 package batch
 
 import (
-	"fmt"
-	"github.com/rightlag/slate/command"
 	"testing"
+
+	"github.com/rightlag/slate"
+	"github.com/rightlag/slate/command"
 )
 
 func TestCommandBatch(t *testing.T) {
-	ls := command.NewCommand("ls -al")
-
 	b := NewBatchCommand()
-	b.Add(ls)
-	if b.commands[0] != ls {
+	b.Add(command.NewCommand("ls -al", nil))
+	if b.Length() != 1 {
+		t.Error()
+	}
+	// If `nil` is the second argument to `NewCommand`,
+	// then the command is ignored.
+	if b.List()[0].Ignore() {
 		t.Error()
 	}
 }
 
 func TestCommandsBatch(t *testing.T) {
-	pwd := command.NewCommand("pwd")
-	ps := command.NewCommand("ps", "-L")
-	cs := []*command.Command{
-		pwd,
-		ps,
-	}
-
 	b := NewBatchCommand()
-	b.Add(cs)
-	if len(b.commands) != 2 {
+	b.Add([]*command.Command{
+		command.NewCommand("ls -al", slate.Boolean(false)),
+		command.NewCommand("rm -rf /", slate.Boolean(true)),
+	})
+	if b.Length() != 2 {
 		t.Error()
 	}
-	if b.commands[1] != ps {
-		t.Error()
-	}
-}
-
-func TestStringBatch(t *testing.T) {
-	ls := command.NewCommand("ls")
-
-	b := NewBatchCommand()
-	b.Add("ls")
-	if b.commands[0] != ls {
-		t.Error()
-	}
-}
-
-func TestStringsBatch(t *testing.T) {
-	cd := command.NewCommand("cd")
-	ls := command.NewCommand("ls -al")
-	cs := []string{
-		"cd",
-		"ls -al",
-	}
-
-	b := NewBatchCommand()
-	b.Add(cs)
-	if len(b.commands) != 2 {
-		t.Error()
-	}
-	fmt.Print(b.commands)
-
-	if b.commands[0] != cd {
-		t.Error()
-	}
-	if b.commands[1] != ls {
+	// Explicitly ignore the second command.
+	if !b.List()[1].Ignore() {
 		t.Error()
 	}
 }
